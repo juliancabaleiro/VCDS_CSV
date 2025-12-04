9afd0eeda13a44a6f734ad0ea271b37aaffc7ae5
juliancabaleiro <juliancabaleiro@gmail.com>
2025-12-04 00:26:50 -0300

    add some files

---
+++ example.py
@@ -0,0 +1,13 @@
+"""
+@author: Julian Cabaleiro
+@repository: https://github.com/juliancabaleiro/VCDS_CSV
+
+Example how to use VCDS_csv
+"""
+
+from VCDS_csv import dash_app_launch
+
+if "__main__"== __name__:
+ 
+ #launch the dash app
+ dash_app_launch()
